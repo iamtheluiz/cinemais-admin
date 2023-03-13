158c40754c2d8f91e2b24b35e3963cd88260f734
Luiz Vasconcellos <luiz.vasconcellos@konia.com.br>
2023-03-13 16:24:00 -0300

    feat: login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { FormEvent, useState } from "react";
+import { api } from "./services/api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function handleLoginSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const response = await api.post('/auth/login', {
+      email,
+      password
+    })
+
+    console.log("asdai")
+
+    if (response.data.success) {
+      const token = response.data.data.token
+      return;
+    }
+
+    alert("Não foi possível fazer o login! Verifique o usuário e a senha utilizados.")
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-900 p-4">
+      <section id="login" className="max-w-md w-full flex flex-col justify-center">
+        <h1 className="font-bold text-center text-white text-2xl mb-5">Login</h1>
+        <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <form onSubmit={handleLoginSubmit} className="px-5 py-7">
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+            <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" value={email} onChange={event => setEmail(event.currentTarget.value)} />
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+            <input type="password" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" value={password} onChange={event => setPassword(event.currentTarget.value)} />
+            <button type="submit" className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+              <span className="inline-block mr-2">Login</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+              </svg>
+            </button>
+          </form>
+          <div className="py-5">
+            <div className="grid grid-cols-2 gap-1">
+              <div className="text-center sm:text-left whitespace-nowrap">
+                <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-top">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
+                  </svg>
+                  <span className="inline-block ml-1">Forgot Password</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-5">
+          <div className="grid grid-cols-2 gap-1">
+            <div className="text-center sm:text-left whitespace-nowrap">
+              <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-top">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                </svg>
+                <span className="inline-block ml-1">Back to your-app.com</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
