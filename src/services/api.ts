c25b8162dd60e2b5950a18dad2a4e44c09200f91
iamtheluiz <iamtheluiz.dev@gmail.com>
2023-06-22 14:52:44 -0300

    feat: change api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://192.168.0.100:3000"
+  // baseURL: "http://192.168.0.100:3000"
   // baseURL: "https://cinemais-api.luizvasconcellos.com.br"
+  baseURL: "https://cinemais-api.onrender.com"
 })
